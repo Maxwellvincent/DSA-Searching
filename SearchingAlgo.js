1fd42e3b4ac99b312da1e50a3b7eb1cac5a78466
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-12-09 18:25:40 -0500

    working Searching problems

---
+++ SearchingAlgo.js
@@ -0,0 +1,30 @@
+//1.  3 searches, 5 seaches
+
+//2. 
+
+//3. Find a book 
+    // I would start by comparing the values to see if it's higher or lesser than the value I am searching for. Traverse down the list until a find the value that I am looking for. 
+
+//4.                79
+                35      90
+            25     27  89  91
+           15 19
+        14
+        // 14 , 145, 19 , 25 , 27 , 89 , 91 , 79
+
+
+                8
+            6       10 
+           5  7    9   11
+
+//5. Create a BST class
+class BST {
+    constructor(value){
+        this.root = value;
+    }
+    inOrder(){}
+    preOrder(){}
+    postOrder(){}
+}
+
+//6. 
